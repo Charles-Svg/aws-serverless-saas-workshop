6ebfac13c90c193f8ec3646e6b81d8572ac541cf
Charles-Svg <78506028+Charles-Svg@users.noreply.github.com>
2025-01-12 20:59:17 +0100

    End of building lab2

--- Lab2/client/Admin/src/aws-exports.ts
+++ Lab2/client/Admin/src/aws-exports.ts
@@ -1,8 +1,8 @@
 const awsmobile = {
-  aws_project_region: 'us-west-2',
-  aws_cognito_region: 'us-west-2',
-  aws_user_pools_id: 'us-west-2_nWhwjijMc',
-  aws_user_pools_web_client_id: '61ipvhmvdrfso0cftpil9irk8k',
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_zpUtYRb8h',
+  aws_user_pools_web_client_id: '3fo630ijts5cl6hhs8bo625efi',
 };
 
 export default awsmobile;
--- Lab2/client/Admin/src/environments/environment.prod.ts
+++ Lab2/client/Admin/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://duobieudcl.execute-api.us-west-2.amazonaws.com/prod',
+  apiUrl: 'https://2dpkm2tf08.execute-api.us-east-1.amazonaws.com/prod',
 };
